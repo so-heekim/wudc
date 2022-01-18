d0de4e0ce99e1b0e0c3c8b631e21435df2cc5c37
so-heekim <sohee.sefo@gmail.com>
2022-01-18 10:38:12 +0900

    Initial commit

---
+++ Android/Study1/app/src/main/java/com/study/study1/Sub1.java
@@ -0,0 +1,14 @@
+package com.study.study1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sub1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub1);
+    }
+}
